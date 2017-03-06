56d75d385d20a491f13d01a1dfb887a8580603a4
Becca <cca.king@gmail.com>
2017-03-06 12:54:49 -0700

    second

--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var User = require('../models/users')
+var SendBird = require('sendbird'); //grabbing what we downloaded
+var sb = SendBird('<b717f925e886e814d7170ba399df8f4db0590566>'); 
+var User = require('../models/users') //importing users
 var app = express();
 
 mongoose.connect('mongodb://localhost/doghouse')
 
 app.get('/test', function(req, res){
-	new User({
+	new User({							//creating user object
 		name: 'Nathan',
 		password: 'guest'
 	}).save(function(err, response){
