5dcc076afe0f1f76e3a8c5ad8997c13df3bf139c
Becca <cca.king@gmail.com>
2017-03-10 13:19:31 -0700

    popn bottles cuzza lat and lng

--- controllers/users.js
+++ controllers/users.js
@@ -8,30 +8,31 @@ function create (req, res){
 	var first = "http://maps.googleapis.com/maps/api/geocode/json?address=";
 	var last = "&AIzaSyDZImnAo3t9Ye0cjExfCq_0mc38ngMS7lM";
 	var geoURL = first.concat(addyPlus).concat(last);
-	var user = new User ({
-	username: req.body.username,
-	password: req.body.password,
-	address: req.body.address,
-	about: req.body.about,
-	dogname: req.body.dogname,
-	breed: req.body.breed,
-	anything: req.body.anything,
-	lat: location.lat,
-	lng: location.lng
-	})
 	request
 		.get(geoURL)
 		.end(function(err, geoRes){
 	   		var location = JSON.parse(geoRes.text).results[0].geometry.location;
+			var user = new User ({
+				username: req.body.username,
+				password: req.body.password,
+				address: req.body.address,
+				about: req.body.about,
+				dogname: req.body.dogname,
+				breed: req.body.breed,
+				anything: req.body.anything,
+				lat: location.lat,
+				lng: location.lng
+			})
 			user.save(function(err, result){
 				if (err) {
-					res.status(500)
-					res.json(err)
-				} else {
-					res.send(result);
+				res.status(500)
+			res.json(err)
+			} else {
+				res.send(result);
 				}
 			})
-	})
+		})
+
 }		
 
 module.exports = {
