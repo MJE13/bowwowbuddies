004c585217fd2eb2ef69ff529fe1fd63c80a213c
morgan <morgan.eichwald@gmail.com>
2017-03-19 20:59:46 -0600

    username and imgURL loads in homepage

--- src/Homepage.js
+++ src/Homepage.js
@@ -11,26 +11,36 @@ export default class Homepage extends Component {
 			profile: {}
 		};
 	}
+
 recieveProfile(){
-	var self = this
-    $.get('http://localhost:3001/api/profile',
-          // imgURL: this.state.imgURL,	
-          // username: this.state.username, //recieve messages from the 'to' field
-          {token: this.props.token}, //checking token to authenticate the user for the ajax call to go through
-        function(response){ 
-        	console.log(response)
-            self.setState({profile: response});
-        })
-  }
+var self = this
+$.get('http://localhost:3001/api/profile',
+      // imgURL: this.state.imgURL,	
+      // username: this.state.username, //recieve messages from the 'to' field
+      {token: this.props.token}, //checking token to authenticate the user for the ajax call to go through
+    function(response){ 
+    	console.log(response)
+        self.setState({profile: response});
+    })
+}
+
 
 	render() {
+		var self = this
+		$.get('http://localhost:3001/api/profile',
+		      // imgURL: this.state.imgURL,	
+		      // username: this.state.username, //recieve messages from the 'to' field
+		      {token: this.props.token}, //checking token to authenticate the user for the ajax call to go through
+		    function(response){ 
+		    	console.log(response)
+		        self.setState({profile: response});
+		})
 		return(
 
 				<div className="Homepage">
 					<h1>Welcome to Bow Wow Buddies!</h1>
 					<br/>
 					<div>
-						<button onClick={this.recieveProfile.bind(this)}>Submit</button><br/>
 					  <HomePane profile={this.state.profile} /> 
 					</div>
 					<a href="./profile">Edit Profile</a><br/>
