dfb06cdb35ab728005b1984cb8bd72d1d1f5d869
Becca <cca.king@gmail.com>
2017-03-22 15:39:24 -0600

    leading slash bug fixed

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ var dev = process.env.BWBDEV ? true : false
 module.exports = {
 	'secret': 'eichwaldcarlsonking',
 	'database': 'mongodb://localhost/doghouse',
-	'apiUrl' : dev ? 'http://localhost:3001' : '/'
+	'apiUrl' : dev ? 'http://localhost:3001' : ''
 };
 
